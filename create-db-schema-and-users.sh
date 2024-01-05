09179d787587c9d1bb55383014153603699f16fb
subhayu1 <subhayu.rimal@gmail.com>
2024-01-05 11:04:38 -0700

    Revert "add gradle build"
    This reverts commit e20fce80

--- create-db-schema-and-users.sh
+++ create-db-schema-and-users.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 #run docker-exec
-docker exec expense-manager-db mysql --user=root --password=password --database=expense_manager< src/main/resources/create-schema-users.sql | exit 0
+docker exec -it expense-manager-db mysql --user=root --password=password --database=expense_manager < src/main/resources/create-schema-users.sql | exit 0
