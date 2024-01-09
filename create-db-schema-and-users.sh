1b4b45c906dc1d86121083971ebfc7d4d3f5789e
subhayu1 <subhayu.rimal@gmail.com>
2024-01-09 08:57:47 -0700

    EXP-21 #comment attempting to fix db creation scripts

--- create-db-schema-and-users.sh
+++ create-db-schema-and-users.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-#run docker-exec
-docker exec expense-manager-db mysql --user=root --password=password< src/main/resources/create-schema-users.sql
+docker exec expense-manager-db mysql --user=root --password=password < src/main/resources/create-schema-users.sql
+docker exec expense-manager-db mysql --user=root --password=password -e "CALL CreateDatabaseAndUsers()"
