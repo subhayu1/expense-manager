75edb6cf1bba2667ad69ced5492456ee37585b7b
subhayu1 <subhayu.rimal@gmail.com>
2024-01-09 08:52:52 -0700

    EXP-15 #comment gracefully handle errors when dependent entities are deleted

--- create-db-schema-and-users.sh
+++ create-db-schema-and-users.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
-docker exec expense-manager-db mysql --user=root --password=password < src/main/resources/create-schema-users.sql
+
+docker exec expense-manager-db mysql --user=root --password=password< src/main/resources/create-schema-users.sql
 docker exec expense-manager-db mysql --user=root --password=password -e "CALL CreateDatabaseAndUsers()"
