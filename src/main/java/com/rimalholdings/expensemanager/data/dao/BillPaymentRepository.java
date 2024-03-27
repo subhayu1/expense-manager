be650c223ec3868b450b779a21ae8456245cff40
subhayu1 <subhayu.rimal@gmail.com>
2024-03-27 17:41:09 -0600

    EXP-44 modify prepareObject query

--- src/main/java/com/rimalholdings/expensemanager/data/dao/BillPaymentRepository.java
+++ src/main/java/com/rimalholdings/expensemanager/data/dao/BillPaymentRepository.java
@@ -25,8 +25,8 @@ Page<BillPaymentEntity> findByVendorId(Long vendorId, Pageable pageable);
 			+ "e.integrationId AS appliesToInvoiceId, e.externalInvoiceNumber AS appliesToInvoiceNumber, "
 			+ "e.description AS description, bp.paymentamount AS amount "
 			+ "FROM billpayment bp "
-			+ "INNER JOIN billpayment_expense bpe ON bp.id = bpe.billpaymentid "
-			+ "INNER JOIN expense e ON bpe.expenseid = e.id "
+			+ "INNER JOIN appayment ap on bp.appaymentid = ap.id "
+			+ "INNER JOIN expense e on ap.id = e.appaymentid "
 			+ "INNER JOIN vendor v ON e.vendorId = v.id "
 			+ "WHERE e.externalorgid = :orgId AND bp.toSync = 1",
 	nativeQuery = true)
