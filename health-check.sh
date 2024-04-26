22b058c954ef7872030392739c2baf892a0822e4
subhayu1 <subhayu.rimal@gmail.com>
2024-04-26 14:48:00 -0600

    attempt #2 to run the build on github runner

---
+++ health-check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Directory where your git repository is located
+REPO_DIR="/path/to/your/repo"
+# JSON file to append the data
+OUTPUT_FILE="/path/to/output.json"
+
+# Navigate to the repository directory
+cd "$REPO_DIR"
+
+# Get the latest commit hash
+commit_hash=$(git rev-parse HEAD)
+
+# Get the current branch name
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+# Get the commit message, replacing newlines with spaces
+commit_message=$(git log -1 --pretty=%B | tr '\n' ' ')
+
+# Get the commit date and time
+commit_date=$(git log -1 --format=%cd)
+
+# Manually create a JSON object
+json_object="{\"commit_hash\": \"$commit_hash\", \"branch_name\": \"$branch_name\", \"commit_message\": \"$commit_message\", \"commit_date\": \"$commit_date\"}"
+
+# Append the JSON object to the output file
+echo $json_object >> $OUTPUT_FILE
